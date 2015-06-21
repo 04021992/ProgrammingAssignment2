a7eb7faf4ea62e1ad6328345a59cee991dfb2931
04021992 <arangnek@hawk.iit.edu>
2015-06-21 13:02:27 -0500

    Update cachematrix.R
    Cache checking

--- cachematrix.R
+++ cachematrix.R
@@ -28,4 +28,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        The cacheSolve<-function(x)
+{
+	m<-x$getinverse()
+	if(!is.null(m))
+	{
+		message("getting cached data")
+		return(m)
+	}
+	data<-x$get()	
+	m<-solve(data)
+	x$setinverse(m)
+	
+    m
+}
+
+
 }
